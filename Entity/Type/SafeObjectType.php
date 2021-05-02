26765df94d157e662bb829c092785c9a341c39a9
Ivan Pavlov <i.pavlov@trackad.ai>
2021-05-02 20:08:47 +0300

    jms_job_safe_object doctrine type commented fix

--- Entity/Type/SafeObjectType.php
+++ Entity/Type/SafeObjectType.php
@@ -2,6 +2,7 @@
 
 namespace JMS\JobQueueBundle\Entity\Type;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\ObjectType;
 
 class SafeObjectType extends ObjectType
@@ -14,5 +15,13 @@ class SafeObjectType extends ObjectType
     public function getName()
     {
         return 'jms_job_safe_object';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function requiresSQLCommentHint(AbstractPlatform $platform)
+    {
+        return false;
     }
 }
