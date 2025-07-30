3bcf693da4390833f7848ac42ec2254ecf75946e
Alexey Nesvetaev <a.d.nesvetaev@gmail.com>
2025-07-30 12:00:30 +0300

    Replaced json_array with json in JobManager.

--- Entity/Repository/JobManager.php
+++ Entity/Repository/JobManager.php
@@ -75,7 +75,7 @@ class JobManager
 
         $firstJob = $this->getJobManager()->createQuery("SELECT j FROM ".Job::class." j WHERE j.command = :command AND j.args = :args ORDER BY j.id ASC")
              ->setParameter('command', $command)
-             ->setParameter('args', $args, 'json_array')
+             ->setParameter('args', $args, Types::JSON)
              ->setMaxResults(1)
              ->getSingleResult();
 
